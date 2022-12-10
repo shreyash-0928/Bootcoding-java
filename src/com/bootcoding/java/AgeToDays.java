c7eab75d1870d8c09294086a6a4fe03aac3a726f
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2022-12-10 13:20:03 +0530

    Program for converting age to days.

---
+++ src/com/bootcoding/java/AgeToDays.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java;
+
+public class AgeToDays {
+    public static void main(String[] args) {
+        int age = 5;
+        int years;
+        years = age * 365;
+        System.out.println("years = " + years);
+    }
+}
