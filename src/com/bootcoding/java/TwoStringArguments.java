de2bb98e318150df672131d86b156452bba4c69a
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2022-12-10 13:31:47 +0530

    Program of two string arguments

---
+++ src/com/bootcoding/java/TwoStringArguments.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java;
+
+public class TwoStringArguments {
+        public static void main(String[] args) {
+        String str1 = "Haldiram";
+        String str2 = "Balaji";
+
+        int a = str1.length();
+        int b = str2.length();
+
+        if (str1 == str2) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
+
