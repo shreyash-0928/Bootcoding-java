420a147fdeb017be18850447da687646f99feef1
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2022-12-10 13:34:54 +0530

    Program of positive numbers in array

---
+++ src/com/bootcoding/java/PositiveNumberInArray.java
@@ -0,0 +1,21 @@
+package com.bootcoding.java;
+
+public class PositiveNumberInArray {
+    public static void main(String[]args){
+        System.out.println("Sum of given array is " + sum());
+    }
+    public static int sum(){
+        int sum = 0;
+        int i;
+        int[] arr = {1,5,-3,2,11};
+
+        for(i=0; i<arr.length; i++){
+            if (arr[i]>0){
+                sum += arr[i];
+            }
+        }
+
+        return sum;
+    }
+
+}
