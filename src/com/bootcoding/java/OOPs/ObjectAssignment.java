13b852c681914cc9356e6231938b2840987d8500
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2022-12-12 09:40:38 +0530

    How to create Objects and final veriable

---
+++ src/com/bootcoding/java/OOPs/ObjectAssignment.java
@@ -0,0 +1,20 @@
+package com.bootcoding.java.OOPs;
+
+public class ObjectAssignment {
+    public static void main(String[] args) {
+        // primitive data types
+
+        int a = 10;
+        float b = a;  // b <- a (value of a)
+        System.out.println("Value of A : " + a);
+        System.out.println("Value of B : " + b);
+
+        //ADT - Abstract data type (Object)
+        Marker blueMarker = new Marker();
+        Marker redMarker = blueMarker;    //redMarker <- BlueMarker (Reference)
+        blueMarker.color = "Blue";
+        redMarker.color = "Red";
+        System.out.println("Value of BlueMarker = " + blueMarker.color);
+        System.out.println("Value of RedMarker = " + redMarker.color);
+    }
+}
---
+++ src/com/bootcoding/java/keywords/FinalKeyword.java
@@ -0,0 +1,27 @@
+package com.bootcoding.java.keywords;
+
+import com.bootcoding.java.OOPs.Marker;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+
+        /*
+         * final keyword is used to create constant variable.
+         * final variable can not be modified once it is initialized
+         *
+         */
+
+        final int a = 10;
+        //a = 20; // not allowed
+        //a = 30; // not allowed
+        final double PI = 3.14;
+        // PI = 2.14 // not allowed
+        // final keyword to object (reference variable)
+        final Marker blueMarker = new Marker();
+        // blueMarker = new Marker(); // not allowed
+        // It is allowed to change property of an Object
+        blueMarker.color = "Blue"; // allowed
+        blueMarker.color = "RED"; // allowed
+    }
+}
+
