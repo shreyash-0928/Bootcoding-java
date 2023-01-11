7d9d8f1bc06e1872dcc71ed4c6c58579719c97a8
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-11 22:29:33 +0530

    Inheritance

---
+++ src/com/bootcoding/java/OOPs/inheritance/Cycle.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java.OOPs.inheritance;
+
+public class Cycle {
+
+    String cycleType;
+    double price;
+    String color;
+    double topSpeed;
+
+    public void createCycle(){
+        System.out.println("Creating a cycle");
+    }
+    public void move(){
+        System.out.println("Moveable");
+    }
+
+
+}
