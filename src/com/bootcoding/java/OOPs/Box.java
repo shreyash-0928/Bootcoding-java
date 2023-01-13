45508c0977b73ed7f3db96ec02c653279b4c6719
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-13 09:48:42 +0530

    Box Application

---
+++ src/com/bootcoding/java/OOPs/Box.java
@@ -0,0 +1,26 @@
+package com.bootcoding.java.OOPs;
+
+public class Box {                        //Duster, Helmet, Cycle
+
+    double length;
+    double breadth;
+    double height;
+
+
+    public static Box createBox(double l, double b, double h){
+        Box box = new Box();
+        box.length = l;
+        box.breadth = b;
+        box.height = h;
+
+        return box;
+    }
+
+    public void print(){
+        System.out.println("Box Length : " + length);
+        System.out.println("Box Breadth : " + breadth);
+        System.out.println("Box Height : " + height);
+
+    }
+
+}
