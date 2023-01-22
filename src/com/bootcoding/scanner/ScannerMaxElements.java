6c0b1df181dfbf4524cae6a58c4a404000f564a7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-22 15:09:46 +0530

    Finding Max elements Using Scanner method

---
+++ src/com/bootcoding/scanner/ScannerMaxElements.java
@@ -0,0 +1,42 @@
+package com.bootcoding.scanner;
+
+import java.util.Scanner;
+
+public class ScannerMaxElements {
+    public  static int maxElement(int[] numbers){
+        int temp=numbers[0];
+        for (int i = 0; i <numbers.length ; i++) {
+            for (int j = i+1; j < numbers.length ; j++) {
+                if(numbers[j]>temp){
+                    temp=numbers[j];
+                }
+            }
+
+        }
+        return temp;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+
+        int[] numbers= new int[size];
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.println("Enter the element "+ i);
+            int number= sc.nextInt();
+            numbers[i]=number;
+
+        }
+
+        //method calling for maxElement
+        int result = maxElement(numbers);
+        System.out.println(result);
+
+
+
+
+    }
+
+}
+
