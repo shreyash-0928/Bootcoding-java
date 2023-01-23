52d4ad0c9c0386c1e29fbef9962b502e7f7e1bfb
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-23 22:34:27 +0530

    Moving Zero

--- src/com/bootcoding/leetcode/MoveZeroes.java
+++ src/com/bootcoding/leetcode/MoveZeroes.java
@@ -1,7 +1,24 @@
 package com.bootcoding.leetcode;
 
 public class MoveZeroes {
-
-    // public static int moveZeroes(int[] nums)
-
+        public void moveZeroes(int[] nums) {
+            int ln = nums.length-1, count=0, j=0;
+            for (int i=0; i<=ln; i++) {
+                if(nums[i]!=0){
+                    if(count==0){
+                        j++;
+                    } else{
+                        nums[j]=nums[i];
+                        j++;
+                    }
+                } else {
+                    count++;
+                }
+            }
+            while(count>0){
+                nums[ln] = 0;
+                ln--;
+                count--;
+            }
+        }
 }
