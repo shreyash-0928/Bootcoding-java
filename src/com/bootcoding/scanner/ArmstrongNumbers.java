c98ddd0129513275c003811037a7d9ab40e353d9
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-25 00:20:31 +0530

    Armstrong Numbers

---
+++ src/com/bootcoding/scanner/ArmstrongNumbers.java
@@ -0,0 +1,10 @@
+package com.bootcoding.scanner;
+
+import java.util.Scanner;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
