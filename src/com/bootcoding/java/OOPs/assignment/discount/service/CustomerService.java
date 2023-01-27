ea62afdc8965023c8e905a3f30f77fccc5205885
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-27 21:14:58 +0530

    Assignment

---
+++ src/com/bootcoding/java/OOPs/assignment/discount/service/CustomerService.java
@@ -0,0 +1,33 @@
+package com.bootcoding.java.OOPs.assignment.discount.service;
+
+import com.bootcoding.java.OOPs.assignment.discount.Customer;
+import com.bootcoding.java.OOPs.assignment.discount.NameGenerator;
+import com.bootcoding.java.OOPs.assignment.discount.OrderCountGenerator;
+import com.bootcoding.java.OOPs.assignment.discount.VisitCountGenerator;
+
+import java.util.UUID;
+
+public class CustomerService {
+    private static final int LIMIT = 100;
+    public  Customer[] getAllCustomers(){
+        Customer[] customers = new Customer[LIMIT];
+        for(int i = 0; i < LIMIT; i++){
+            Customer customer = buildCustomer();
+            customers[0] = customer;
+        }
+        return customers;
+    }
+
+    private Customer buildCustomer(){
+        Customer customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setName(NameGenerator.getName());
+        customer.setOrderCount(OrderCountGenerator.getOrderCount());
+        customer.setVisitCount(VisitCountGenerator.getVisitCount());
+
+
+        return customer;
+    }
+
+
+}
