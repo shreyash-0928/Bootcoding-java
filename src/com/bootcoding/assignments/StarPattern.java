adeeb9954bc3fa8aa262e53396d17dbc6185c6f9
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-29 23:11:38 +0530

    Star Pattern

--- src/com/bootcoding/assignments/StarPattern.java
+++ src/com/bootcoding/assignments/StarPattern.java
@@ -1,22 +1,13 @@
 package com.bootcoding.assignments;
 
 public class StarPattern {
-    public static void StarRightTriangle(int n)
-    {
-        for(int i = 0; i < 5; i++) {
-
-            for(int j = 0; j < i; j++) {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-
             System.out.println();
         }
     }
-
-    public static void main(String[] args) {
-        int n = 5;
-        StarRightTriangle(n);
-
-    }
-
 }
+
