c3b446a16c95d5e2e74dead8d45f9fae84fda0bd
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-29 23:06:28 +0530

    LeetCode Problem 258

---
+++ src/com/bootcoding/leetcode/AddDigits.java
@@ -0,0 +1,11 @@
+package com.bootcoding.leetcode;
+
+public class AddDigits {
+    public int addDigits(int num) {
+
+        if(num<10){
+            return num;
+        }
+        return addDigits(num/10 + num%10);
+    }
+}
