015c874c04860215a8324f25df5c4f67044919a3
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-04 23:28:23 +0530

    LinkedList Cycle

--- src/com/bootcoding/leetcode/LinkedListCycle.java
+++ src/com/bootcoding/leetcode/LinkedListCycle.java
@@ -1,20 +1,20 @@
-package com.bootcoding.leetcode;
-
-import com.bootcoding.leetcode.ListNode;
-
-public class LinkedListCycle {
-    public boolean hasCycle(ListNode head) {
-
-        ListNode sp = head;
-        ListNode fp = head;
-        while (fp != null && fp.next != null) {
-            sp = sp.next;
-            fp = fp.next.next;
-            if (sp == fp) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-}
+//package com.bootcoding.leetcode;
+//
+//import com.bootcoding.leetcode.ListNode;
+//
+//public class LinkedListCycle {
+//    public boolean hasCycle(ListNode head) {
+//
+//        ListNode sp = head;
+//        ListNode fp = head;
+//        while (fp != null && fp.next != null) {
+//            sp = sp.next;
+//            fp = fp.next.next;
+//            if (sp == fp) {
+//                return true;
+//            }
+//        }
+//        return false;
+//
+//    }
+//}
