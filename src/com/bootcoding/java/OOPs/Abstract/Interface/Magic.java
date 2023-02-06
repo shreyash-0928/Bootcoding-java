166a39daf06f7eec4059f3ed261dd0ac197bda9b
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-06 22:59:36 +0530

    InterFace Example

---
+++ src/com/bootcoding/java/OOPs/Abstract/Interface/Magic.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.OOPs.Abstract.Interface;
+
+public class Magic {
+    public static void showMagic(Sim sim) {
+        System.out.println("Start");
+        sim.inserted();
+        System.out.println("The End");
+    }
+}
