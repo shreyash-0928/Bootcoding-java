7402e8c3487bb2b8be50f8e13824ee72b86bfcd0
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-06 22:56:43 +0530

    LooslyCoupled Example

---
+++ src/com/bootcoding/java/OOPs/Abstract/looslyCoupled/Airtel.java
@@ -0,0 +1,8 @@
+package com.bootcoding.java.OOPs.Abstract.looslyCoupled;
+
+class Airtel extends Sim {
+    public void inserted()
+    {
+        System.out.println("Airtel sim inserted");
+    }
+}
