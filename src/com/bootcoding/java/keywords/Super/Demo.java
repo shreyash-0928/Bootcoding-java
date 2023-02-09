50983a6faf093a5d64cbbe9c4391497cdbe63ae7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-09 19:14:45 +0530

    Super Keyword example

---
+++ src/com/bootcoding/java/keywords/Super/Demo.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.keywords.Super;
+
+class Demo {
+    public static void main(String[] args) {
+        A a1 = new A(100);
+        B b1 = new B(200);
+        C c1 = new C(300);
+
+    }
+}
