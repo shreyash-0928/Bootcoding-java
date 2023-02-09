0d0af2586ed58d831caff8bb366b6ad8ab62f010
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-09 19:13:55 +0530

    Super Keyword example

---
+++ src/com/bootcoding/java/keywords/Super/A.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.keywords.Super;
+
+class A {
+    A()
+    {
+        System.out.println("Default Constructor From A");
+    }
+    A(int n)
+    {
+        System.out.println("Parameterized Constructor From A");
+    }
+}
