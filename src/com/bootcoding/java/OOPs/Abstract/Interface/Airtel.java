b7b88c1136ad087a92c199234c124801edee07e6
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-09 19:16:16 +0530

    InterFace example

---
+++ src/com/bootcoding/java/OOPs/Abstract/Interface/Airtel.java
@@ -0,0 +1,8 @@
+package com.bootcoding.java.OOPs.Abstract.Interface;
+
+class Airtel implements Sim {
+    public void inserted()
+    {
+        System.out.println("Airtel sim inserted");
+    }
+}
