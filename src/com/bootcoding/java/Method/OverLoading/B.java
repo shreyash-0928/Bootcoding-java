816fe41db5d7737c27f61921751050675a1a9810
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-10 23:15:46 +0530

    OverLoading example

---
+++ src/com/bootcoding/java/Method/OverLoading/B.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.Method.OverLoading;
+
+public class B extends A{
+    void showB(){
+        System.out.println("This is show B");
+    }
+    void show(){
+        System.out.println("I am in show B");
+    }
+}
