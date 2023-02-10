d9e15fec0f180def7998b8113ad469d95fab15fa
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-10 23:15:33 +0530

    OverLoading example

---
+++ src/com/bootcoding/java/Method/OverLoading/A.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.Method.OverLoading;
+
+public class A {
+    void showA(){
+        System.out.println("This is show A");
+    }
+    void show(){
+        System.out.println("I am in A");
+    }
+}
