28333db856d40cc490251f12713497f4f8c71b89
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-14 23:14:18 +0530

    Duster Object

---
+++ src/com/bootcoding/java/OOPs/object/Duster.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.OOPs.object;
+
+public class Duster {
+    double Price;
+    String Shape;
+    String features;
+
+    public void print(){
+        System.out.println(" Purchasing new duster. ");
+        System.out.println(" Duster price " + Price);
+        System.out.println(" Shape of duster " + Shape);
+        System.out.println(" Features of duster " + features);
+    }
+
+
+}
