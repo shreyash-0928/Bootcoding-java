bf4f01cd8281de47684abdefc974002e2502e72d
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-16 22:09:50 +0530

    Example of object Box

--- src/com/bootcoding/java/OOPs/object/Box.java
+++ src/com/bootcoding/java/OOPs/object/Box.java
@@ -1,26 +1,27 @@
 package com.bootcoding.java.OOPs.object;
 
-public class Box {                        //Duster, Helmet, Cycle
+//public class Box {                        //Duster, Helmet, Cycle
+//
+//    double length;
+//    double breadth;
+//    double height;
+//
+//
+//    public static Box createBox(double l, double b, double h){
+//        Box box = new Box();
+//        box.length = l;
+//        box.breadth = b;
+//        box.height = h;
+//
+//        return box;
+//    }
+//
+//    public void print(){
+//        System.out.println("Box Length : " + length);
+//        System.out.println("Box Breadth : " + breadth);
+//        System.out.println("Box Height : " + height);
+//
+//    }
+//
+//}
 
-    double length;
-    double breadth;
-    double height;
-
-
-    public static Box createBox(double l, double b, double h){
-        Box box = new Box();
-        box.length = l;
-        box.breadth = b;
-        box.height = h;
-
-        return box;
-    }
-
-    public void print(){
-        System.out.println("Box Length : " + length);
-        System.out.println("Box Breadth : " + breadth);
-        System.out.println("Box Height : " + height);
-
-    }
-
-}
