d833a71b1fda8e8ab9124f7e17e2605370b48893
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-19 23:40:37 +0530

    LeetCode Problem 283

--- src/com/bootcoding/leetcode/MoveZeroes.java
+++ src/com/bootcoding/leetcode/MoveZeroes.java
@@ -1,24 +1,35 @@
 package com.bootcoding.leetcode;
 
+import java.util.Arrays;
+
 public class MoveZeroes {
-        public void moveZeroes(int[] nums) {
-            int ln = nums.length-1, count=0, j=0;
-            for (int i=0; i<=ln; i++) {
-                if(nums[i]!=0){
-                    if(count==0){
-                        j++;
-                    } else{
-                        nums[j]=nums[i];
-                        j++;
-                    }
-                } else {
-                    count++;
-                }
+
+    public static void moveZerosToEnd(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+
+        // move all non-zero elements to the front
+        while (i < n) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
             }
-            while(count>0){
-                nums[ln] = 0;
-                ln--;
-                count--;
-            }
+            i++;
         }
+
+        // fill the remaining elements with zeros
+        while (j < n) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 3, 3};
+        moveZerosToEnd(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
 }
+
