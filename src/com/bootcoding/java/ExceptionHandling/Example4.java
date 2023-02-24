7c1fb685dc7dc607c72a63a10ffac34467759aa3
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-24 23:22:19 +0530

    Example 4

---
+++ src/com/bootcoding/java/ExceptionHandling/Example4.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.ExceptionHandling;
+
+public class Example4 {
+    public static void main(String args[])
+    {
+        System.out.println("I am in main");
+        Example4 example4= new Example4();
+        example4.show();
+
+    }
+    public void show()
+    {
+        System.out.println("I am in show");
+    }
+}
