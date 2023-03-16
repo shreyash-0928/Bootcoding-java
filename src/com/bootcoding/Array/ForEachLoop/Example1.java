1ad5ecb67a434206c5d0019743b70356761d6236
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-16 23:40:53 +0530

    For Each Loop Example 1

---
+++ src/com/bootcoding/Array/ForEachLoop/Example1.java
@@ -0,0 +1,11 @@
+package com.bootcoding.Array.ForEachLoop;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,6,7};
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
+
