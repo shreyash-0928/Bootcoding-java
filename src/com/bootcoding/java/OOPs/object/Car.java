c4bff079c80e90d8ff4896fbbc2aba4a0a8991cd
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-16 23:45:29 +0530

    Car Example

--- src/com/bootcoding/java/OOPs/object/Car.java
+++ src/com/bootcoding/java/OOPs/object/Car.java
@@ -9,28 +9,24 @@ public class Car {
     public String seats;
     public String fuelType;
 
-    public void startEngine(){
+    public void startEngine() {
         System.out.println("Car started");
     }
 
-    public void stopEngine(){
+    public void stopEngine() {
         System.out.println("Car Stopped");
     }
 
-    public void move(){
+    public void move() {
         System.out.println("Car is moving");
     }
 
-    public void print(){
+    public void print() {
         System.out.println("Car Brand : " + brandName);
         System.out.println("Car Model : " + model);
         System.out.println("Car Color : " + color);
         System.out.println("Car Lights : " + lights);
         System.out.println("Car Seats : " + seats);
         System.out.println("Car FuelType : " + fuelType);
-
-
     }
-
-
 }
