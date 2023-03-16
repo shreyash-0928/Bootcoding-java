3b0b992e8f4aad7a5bf5170cff4a78f24bed3044
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-16 23:42:43 +0530

    Example 3

---
+++ src/com/bootcoding/Array/ForLoop/Example3.java
@@ -0,0 +1,12 @@
+package com.bootcoding.Array.ForLoop;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int a[] = {2,5,7,10,12};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+
+        }
+    }
+
+}
