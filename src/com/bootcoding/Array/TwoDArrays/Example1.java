b6b82afaabe139f384514440ac86565a38f2edd4
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-19 23:56:35 +0530

    Example 1

---
+++ src/com/bootcoding/Array/TwoDArrays/Example1.java
@@ -0,0 +1,13 @@
+package com.bootcoding.Array.TwoDArrays;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int a[][] = new int[3][4];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(a[i][j]+"");
+            }
+            System.out.println();
+        }
+    }
+}
