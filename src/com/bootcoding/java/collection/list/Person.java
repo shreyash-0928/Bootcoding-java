b5a38bfcfff21bdde0cdcfdb7eef9f5e11224646
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-27 23:35:56 +0530

    Person class

---
+++ src/com/bootcoding/java/collection/list/Person.java
@@ -0,0 +1,46 @@
+package com.bootcoding.java.collection.list;
+
+public class Person {
+    private String name;
+    private int age;
+    private long phone;
+
+    public Person(String name, int age, long phone) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", phone=" + phone +
+                '}';
+    }
+}
