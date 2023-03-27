30a43219b4cf9f87ece4a1d65e82bbfc958ea2d8
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-27 23:32:56 +0530

    Application

--- src/com/bootcoding/leetcode/Application.java
+++ src/com/bootcoding/leetcode/Application.java
@@ -1,12 +1,12 @@
-package com.bootcoding.leetcode;
-
-public class Application {
-    public static void main(String[] args) {
-        RunningSumOfArray sum = new RunningSumOfArray();
-        int[] nums = {1, 2, 3, 4};
-        int[] runningSum = sum.getRunningSum(nums);
-        for(int i = 0; i< nums.length; i++){
-            System.out.println(runningSum);
-        }
-    }
-}
+//package com.bootcoding.leetcode;
+//
+//public class Application {
+//    public static void main(String[] args) {
+//        RunningSumOfArray sum = new RunningSumOfArray();
+//        int[] nums = {1, 2, 3, 4};
+//        int[] runningSum = sum.getRunningSum(nums);
+//        for(int i = 0; i< nums.length; i++){
+//            System.out.println(runningSum);
+//        }
+//    }
+//}
