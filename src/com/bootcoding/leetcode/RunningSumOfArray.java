799862ac342ea681ae306110ed0364f8f1e378a7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-03-27 23:33:21 +0530

    Running Sum Of Array

--- src/com/bootcoding/leetcode/RunningSumOfArray.java
+++ src/com/bootcoding/leetcode/RunningSumOfArray.java
@@ -1,24 +1,23 @@
-package com.bootcoding.leetcode;
-
-public class RunningSumOfArray {
-    public static int[] getRunningSum(int[] nums){
-        int i = 1;
-        while(i<nums.length)
-        {
-            nums[i] += nums[i-1];
-            i++;
-        }
-        return nums;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-        int[] runningSum = getRunningSum(nums);
-        for (int i = 0; i < runningSum.length; i++) {
-            System.out.print(runningSum[i] + " ");
-        }
-
-    }
-
-}
-
+//package com.bootcoding.leetcode;
+//
+//public class RunningSumOfArray {
+//    public static int[] getRunningSum(int[] nums){
+//        int i = 1;
+//        while(i<nums.length)
+//        {
+//            nums[i] += nums[i-1];
+//            i++;
+//        }
+//        return nums;
+//    }
+//
+//    public static void main(String[] args) {
+////        int[] runningSum = getRunningSum(nums);
+//        for (int i = 0; i < runningSum.length; i++) {
+//            System.out.print(runningSum[i] + " ");
+//        }
+//
+//    }
+//
+//}
+//
