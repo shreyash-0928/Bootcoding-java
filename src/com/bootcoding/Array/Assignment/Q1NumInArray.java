f3da496a054e029d93f3bfb26e1be66c0ae1db2c
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-04 23:46:07 +0530

    Q1 Store following values into an array and print them to console.

---
+++ src/com/bootcoding/Array/Assignment/Q1NumInArray.java
@@ -0,0 +1,11 @@
+package com.bootcoding.Array.Assignment;
+
+public class Q1NumInArray {
+    public static void main(String[] args) {
+        int[] a = {10,15,20,25,30};
+
+        for (int num : a) {
+            System.out.println(num);
+        }
+    }
+}
