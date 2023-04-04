4996fd6aad436309a192a9eec0c345776a81d7f3
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-04 23:47:33 +0530

    Q2 Store following values into an array and print them to console.

---
+++ src/com/bootcoding/Array/Assignment/Q2StringInArray.java
@@ -0,0 +1,10 @@
+package com.bootcoding.Array.Assignment;
+
+public class Q2StringInArray {
+    public static void main(String[] args) {
+        String[] a = {"Apple","Avocado","Grapes","Pineapple","Watermelon"};
+        for (String s : a) {
+            System.out.println(s);
+        }
+    }
+}
