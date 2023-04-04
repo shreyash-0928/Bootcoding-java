7c62a8eeddbd2df189ba689afe15cbeddf202383
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-04 23:47:55 +0530

    Q3 Store all month names into an array in the sequence and print them to console.

---
+++ src/com/bootcoding/Array/Assignment/Q3MonthsInArray.java
@@ -0,0 +1,11 @@
+package com.bootcoding.Array.Assignment;
+
+public class Q3MonthsInArray {
+    public static void main(String[] args) {
+        String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        for(String MonthNames : monthNames)
+            System.out.println(MonthNames);
+    }
+
+}
