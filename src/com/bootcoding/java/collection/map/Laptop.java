6ed22ff184f9c8800e640f12fd4b0290b8424652
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-06 00:42:57 +0530

    HAshMap

---
+++ src/com/bootcoding/java/collection/map/Laptop.java
@@ -0,0 +1,58 @@
+package com.bootcoding.java.collection.map;
+
+public class Laptop implements Comparable<Laptop>{
+    private String name;
+    private int ram;
+    private double price;
+
+    public Laptop() {
+    }
+
+    public Laptop(String name, int ram, double price) {
+        this.name = name;
+        this.ram = ram;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "name='" + name + '\'' +
+                ", ram=" + ram +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Laptop laptop) {
+        if (this.getRam() > laptop.getRam()){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+}
