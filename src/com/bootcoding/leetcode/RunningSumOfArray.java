8d80893e4993dcd20b5177f2652e32dcc65a54c8
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-07 23:28:57 +0530

    LeetCode Problem 1480

--- src/com/bootcoding/leetcode/RunningSumOfArray.java
+++ src/com/bootcoding/leetcode/RunningSumOfArray.java
@@ -1,23 +1,34 @@
-//package com.bootcoding.leetcode;
+package com.bootcoding.leetcode;
+
+public class RunningSumOfArray {
+    public static void Sum(int [] num){
+        int a[] = new int[num.length];
+
+        for ( int i=0;i<num.length;i++){
+            int sum=0;
+            for (int j=0;j<=i;j++){
+                sum+=num[j];
+            }
+            a[i]=sum;
+            System.out.println(sum);
+        }
+
+
+    }
+
+//    public static void main(String[] args) {
+//        int[] num = {1, 2, 3, 4, 5};
+//        Sum(num);
+//    }
 //
-//public class RunningSumOfArray {
-//    public static int[] getRunningSum(int[] nums){
-//        int i = 1;
-//        while(i<nums.length)
-//        {
-//            nums[i] += nums[i-1];
-//            i++;
+//    public int[] runningSum(int[] nums) {
+//
+//        for(int i=1;i<nums.length;i++){
+//            nums[i]=nums[i]+nums[i-1];
+//
 //        }
 //        return nums;
 //    }
-//
-//    public static void main(String[] args) {
-////        int[] runningSum = getRunningSum(nums);
-//        for (int i = 0; i < runningSum.length; i++) {
-//            System.out.print(runningSum[i] + " ");
-//        }
-//
-//    }
-//
-//}
-//
+
+}
+
