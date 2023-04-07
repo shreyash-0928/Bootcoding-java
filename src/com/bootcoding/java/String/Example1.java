8000cc561209cb82d12557bba47544b9fceb5f58
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-07 23:25:47 +0530

    String Example 1

---
+++ src/com/bootcoding/java/String/Example1.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.String;
+
+public class Example1 {
+    public static void main(String[] args) {
+        String s = "Banti";
+        System.out.println(s);
+
+        s.concat("Babli");
+        System.out.println(s);
+    }
+}
