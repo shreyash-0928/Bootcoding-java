5fe3986ee5bbd0d6113067ca53bed1ae710e2df4
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-10-30 21:11:20 +0530

    Greatest Common Divisor/ HCF All Approaches

--- src/main/java/com/bootcoding/assignments/GCD.java
+++ src/main/java/com/bootcoding/assignments/GCD.java
@@ -6,22 +6,56 @@ import java.util.Scanner;
 public class GCD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        int on1 = n1;
-        int on2 = n2;
+        System.out.println(gcd(20,15));
+        System.out.println(euclidGCD(20,15));
+        System.out.println(optimisedGCD(20,15));
 
-        while(n1 % n2 != 0)
-        {
-            int rem = n1 % n2;
-            n1 = n2;
-            n2 = rem;
+    }
 
+    //Brute-Force Approach   Time Complexity -> O(min(a,b))
+    static int gcd(int a, int b){
+        int min = 0;
+        if(a<b){
+            min = a;
+        }else{
+            min = b;
         }
+        for (int i=min; i>=1; i--) {
+            if(a%i==0 && b%i==0){
+                return i;
+            }
+        }
+        return 1;
+    }
 
-        int gcd = n2;
-        System.out.println(gcd);
-        
+    //Euclid's Approach    Time Complexity -> O(min(a,b))
+    static int euclidGCD(int a, int b){
+        while (a!=b){
+            if(a>b){
+                a = a-b;
+            }else{
+                b = b-a;
+            }
+        }
+        return a;
+    }
+
+    //Optimised Euclid's Approach  Time Complexity -> O(log(min(a,b))
+    static int optimisedGCD(int a, int b){
+        while(a!=0 && b!=0){
+            if(a>b){
+                a = a%b;
+            }else{
+                b = b%a;
+            }
+        }
+        if(a!=0){
+            return a;
+        }else{
+            return b;
+        }
     }
 }
