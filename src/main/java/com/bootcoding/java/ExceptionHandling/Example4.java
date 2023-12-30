23b1a356672dc254752fc359233476e4294110bf
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-12-30 22:52:12 +0530

    Exception Handling examples

--- src/main/java/com/bootcoding/java/ExceptionHandling/Example4.java
+++ src/main/java/com/bootcoding/java/ExceptionHandling/Example4.java
@@ -3,7 +3,7 @@ package com.bootcoding.java.ExceptionHandling;
 public class Example4 {
     public static void main(String args[])
     {
-        System.out.println("I am in main");
+        System.out.println("I am in Test");
         Example4 example4= new Example4();
         example4.show();
 
